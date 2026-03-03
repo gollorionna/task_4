43414f6a25b7d875ef2f92ae38be3c171188cac0
Viktoria Daineko <daineko0404@gmail.com>
2026-03-03 15:31:48 +0300

    fix: change url for img in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
+import image from '../assets/Mask_Group.webp';
 
 export const Route = createFileRoute('/')({
   component: MainPage,
@@ -7,7 +8,7 @@ export const Route = createFileRoute('/')({
 function MainPage() {
   return (
     <>
-      <div className="bg-[url('src/assets/Mask_Group.webp')] h-screen w-screen bg-cover bg-center bg-no-repeat relative pt-3.5"></div>
+      <div className=" h-screen w-screen bg-cover bg-center bg-no-repeat relative pt-3.5" style={{ backgroundImage: `url(${image})` }}></div>
       <div className="absolute top-40 left-170 bg-[#9cd08f]/80 rounded-tr-2xl rounded-bl-2xl p-6 w-1/2 text-center leading-7 max-w-160">
         <p>New Arrival</p>
         <h1 className="text-2xl font-bold">Discover Our New Collection</h1>
