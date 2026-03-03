2ba2c8ebe7186b258691b186cef742e4501a2ccd
Viktoria Daineko <daineko0404@gmail.com>
2026-03-03 15:10:17 +0300

    Updates

---
+++ assets/products-CLKyVJzW.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/ProductCardList-GUNKFJCN.js","assets/index-F5MwCsGx.js","assets/index-CCye524J.css"])))=>i.map(i=>d[i]);
+import{r,_ as o}from"./index-F5MwCsGx.js";const e=r.lazy(()=>o(()=>import("./ProductCardList-GUNKFJCN.js"),__vite__mapDeps([0,1,2])).then(t=>({default:t.ProductCardList})));export{e as component};
