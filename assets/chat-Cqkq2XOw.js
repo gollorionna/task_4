aa812bb1de5fbc13d1d1c71ffb1d1de13e88cb94
Viktoria Daineko <daineko0404@gmail.com>
2026-03-03 15:59:15 +0300

    Updates

---
+++ assets/chat-Cqkq2XOw.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Chat-BmbW7SCr.js","assets/index-Br64GPwb.js","assets/index-ClzW6dhb.css","assets/types-_-O3hTTy.js"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-Br64GPwb.js";const _=o.lazy(()=>t(()=>import("./Chat-BmbW7SCr.js"),__vite__mapDeps([0,1,2,3])));export{_ as component};
