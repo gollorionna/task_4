63c98639e6caa284ca8eee1d03f1c3f2ed3e9d66
Viktoria Daineko <daineko0404@gmail.com>
2026-03-03 15:32:07 +0300

    Updates

--- assets/products-DPFQzwy2.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/ProductCardList-FA9OxpXI.js","assets/index-B_upusZs.js","assets/index-C6CvaRw9.css"])))=>i.map(i=>d[i]);
-import{r,_ as o}from"./index-B_upusZs.js";const e=r.lazy(()=>o(()=>import("./ProductCardList-FA9OxpXI.js"),__vite__mapDeps([0,1,2])).then(t=>({default:t.ProductCardList})));export{e as component};
