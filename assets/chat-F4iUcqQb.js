9c33d91458db4d7cee0f3fe536a606d848351441
Viktoria Daineko <daineko0404@gmail.com>
2026-03-04 10:45:49 +0300

    Updates

---
+++ assets/chat-F4iUcqQb.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Chat-DkVhPGYh.js","assets/index-C3tkuEfg.js","assets/index-ClzW6dhb.css","assets/types-_-O3hTTy.js"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-C3tkuEfg.js";const _=o.lazy(()=>t(()=>import("./Chat-DkVhPGYh.js"),__vite__mapDeps([0,1,2,3])));export{_ as component};
---
+++ assets/products-bevtShGl.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/ProductCardList-DNLi_Fxd.js","assets/index-C3tkuEfg.js","assets/index-ClzW6dhb.css"])))=>i.map(i=>d[i]);
+import{r,_ as o}from"./index-C3tkuEfg.js";const e=r.lazy(()=>o(()=>import("./ProductCardList-DNLi_Fxd.js"),__vite__mapDeps([0,1,2])).then(t=>({default:t.ProductCardList})));export{e as component};
