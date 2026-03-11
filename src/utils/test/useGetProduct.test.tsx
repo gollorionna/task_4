b8ba5ea9c3f4cf70658bfa8ab946cb73fadda7e4
Viktoria Daineko <daineko0404@gmail.com>
2026-03-11 12:58:31 +0300

    feat: create 2 unit-tests

---
+++ src/utils/test/useGetProduct.test.tsx
@@ -0,0 +1,53 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { renderHook, waitFor } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { useGetProducts } from '../useGetProducts';
+
+const mockProducts = [
+  { id: 1, title: 'Phone' },
+  { id: 2, title: 'Laptop' },
+];
+
+describe('useGetProducts', () => {
+
+  beforeEach(() => {
+    vi.spyOn(globalThis, 'fetch').mockResolvedValue({
+      json: async () => ({
+        products: mockProducts,
+      }),
+    } as Response);
+  });
+
+  const createWrapper = () => {
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+
+    return ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    );
+  };
+
+  it('should fetch products successfully', async () => {
+    const { result } = renderHook(() => useGetProducts(), {
+      wrapper: createWrapper(),
+    });
+
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+    });
+
+    expect(result.current.data).toEqual(mockProducts);
+
+    expect(fetch).toHaveBeenCalledWith(
+      'https://dummyjson.com/products'
+    );
+  });
+});
